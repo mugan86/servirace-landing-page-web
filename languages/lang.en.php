8e7a2be92d38847cead11ad4258da695718c8b61
Anartz Muxika Ledo <mugan86@gmail.com>
2016-12-22 13:23:46 +0100

    Start to translate to english language

---
+++ languages/lang.en.php
@@ -0,0 +1,157 @@
+<?php
+/* 
+------------------
+Language: English
+------------------
+*/
+
+$lang = array();
+
+$lang['PAGE_TITLE'] = 'ServiRace - Korrikalari herrikoiaren gida';
+$lang['HEADER_TITLE'] = 'My website header title';
+$lang['SITE_NAME'] = 'My Website';
+$lang['SLOGAN'] = 'My slogan here';
+$lang['HEADING'] = 'Heading';
+
+// Menu
+
+$lang['MENU_HOME'] = 'Home';
+$lang['MENU_WHAT_IS'] = 'Zer da?';
+$lang['MENU_PORTFOLIO'] = 'Portfolio';
+$lang['MENU_FUNCTIONS'] = 'Functions';
+$lang['MENU_TALK_ABOUT'] = 'Hitz egiten da...';
+$lang['MENU_FAQ'] = 'About app';
+$lang['MENU_CONTACT_US'] = 'Contact';
+
+
+// Page Texts
+
+$lang['APP_FUNCTIONS'] = 'Aplikazioaren funtzionalitateak';
+$lang['APP_OPINION_USERS_TITLE'] = 'Zer pentsatzen dute aplikazioari buruz erabiltzaileek?';
+$lang['APP_IMAGES'] = 'Aplikazioari buruzko irudi batzuk';
+$lang['MY_DESCRIPTION_PROFILE'] = 'ServiRace Sortzailea / Garatzailea';
+$lang['DOWNLOAD_APP_FROM_GPLAY'] = 'Jeitsi aplikazioa Google Play-n';
+$lang['WHAT_IS_SERVIRACE_WHAT_OFFER'] = '<h2>Servirace, Zer da? Zer eskaintza?</h2>
+					<div class="name-zig"></div>
+
+					<div class="col-md-12">
+						<p>Servirace zuzentzen den publiko objektiboarentzat erreferentzia izateko jaio da, hau da, korrikalariak, hauen lagunak eta lasterketen antolatzaileak.</p>
+						<p>Denok dakigu kirola egitea onuragarria dela osasunarentzat eta aplikazio hau sortu da helburua hau betetzeko eta beste korrikalariekin sozializatzeko.</p>
+						<p>Lasterketen egutegiaren erreferentzia osoagoa edukitzeko sortu da Servirace webgune askok oinarrizko informazioa ematen digu eta kasu askotan beharrezkoa da jakitea nola heldu ahal garen lasterketa batera (GPS-aren erabileraz gure kokapena hartuta eta Google Maps aplikazioa erabiliz), egingo duen eguraldi iragarpena , lasterketa bati buruz pentsatzen dutena erabiltzaileek lasterketa horretan "merezi ote duen" jakiteko ala ez,...</p>
+						
+					</div>
+					<div class="col-md-12">
+						<p>Informazio gehiena datu irekiekin eta datu multzo probatu askokin lan egiten du, hurrengoak direlarik iturriak: 
+							<ul>
+								<li><a href="http://opendata.euskadi.eus/catalogo-datos/" target="_blank">Open Data Euskadi</a>: Euskal Herriko zerbitzuak hartzeko, autonimi erkidegoen datuak eta gehiago.</li>
+								<li><a href="http://www.lasterketak.eus" target="_blank">Lasterketak.eus</a>: Euskal Herriko eta inguruko lasterketa guztien informazioa.</li>
+								<li><a href="http://openweathermap.org/api" target="_blank">Open Weather Map</a>: Hurrengo egun eta asteko eguraldi iragarpena jasotzeko lasterketa batena.</li>
+								<li><a href="https://developers.google.com/places/web-service/" target="_blank">Google Places</a>: Euskal Herri kanpoko zerbitzuak lortzeko eta Euskal Herrian Open Data datu multzoetatik lortu ez diren datuak osatzeko gasolindegietan, autoen alokailuetan,...</li>
+								<li>Servicio Web Servirace: Servirace buruzu lortzen diren datu desberdinen API iturri pribatu, nik garatutakoanon lortuko diren datuak iritziak, erabiltzaileak,... emango digu pribatuki.</li>
+							</ul>
+						</p>
+						<p>Gaur egunean, hauek dira aplikazioak dituen datuak: 
+							<ul>
+								<li><b>Lasterketak</b>: 980 eta gorantz komunitatearen laguntzari nahiz <a href="http://www.lasterketak.eus" target="_blank">lasterketak.eus</a> webguneari esker.</li>
+								<li><b>Lasterketei erlazionaturiko zerbitzuak</b>: 88000 eta gorantz, lasterketak berriak gehitzen doazen neurrira.</li>
+								<li><b>Iritzi kopurua</b>: 210.</li>
+								<li><b>Izena emandako erabiltzaileak</b>: 365, iritzia eta beste funtzioez gozatzeko erabiltzaileak.</li>
+								<li><b>Google Play-n deskarga kopurua</b>: 1000 deskargatik gora.</li>
+								<li><b>Google Play-n puntuazio orokorra</b>: 4.8 / 5 69 iritzietan.</li>
+							</ul>
+						</p>
+					</div>';
+
+//Talk about servirace
+
+$lang['TALK_ABOUT_APP'] = 'ServiRace aplikazioari buruz hitz egin da...';
+
+//FORMS / CONTACT
+
+$lang['CONTACT_WITH_ME'] = 'Contacta conmigo.';
+$lang['CONTACT_WITH_ME_TEXT'] = 'Puedes contactar conmigo mediante el formulario o desde cualquier red social.';
+$lang['FORM_YOUR_NAME_LABEL'] = 'Zure izena';
+$lang['FORM_YOUR_NAME_PLACEHOLDER'] = 'Sartu zure izena';
+$lang['FORM_MAIL_LABEL'] = 'Posta elektronikoa';
+$lang['FORM_MAIL_PLACEHOLDER'] = 'Sartu zure posta elektronikoa';
+$lang['FORM_MESSAGE_LABEL'] = 'Mezua';
+$lang['FORM_MESSAGE_PLACEHOLDER'] = 'Idatzi zure mezua';
+$lang['FORM_SEND_BUTTON_LABEL'] = 'Bidali';
+$lang['DEVELOPE_BY'] = 'Anartz Mugikak garatua.';
+
+//APP Functions 
+
+$lang['APP_FUNCTIONS_SUBTITLE'] = 'Hauek dira aplikazioaren funtzio nagusiak.';
+$lang['APP_FUNCTIONS_TEXT'] = 'Gehiago aurkitu nahi izanez gero, jeitsi aplikazioa <a href="https://play.google.com/store/apps/details?id=com.amugika.servirace&hl=es" target="_blank">hemendik</a> <b><u>DOHAINIK</u></b> dela.';
+
+$lang['APP_FUNCTIONS_RACE_SEARCH_TITLE'] = '<p>LASTERKETA BILATZAILEA<br/><small>Bilatzaile aurreratu batekin</small></p>';
+$lang['APP_FUNCTIONS_RACE_SEARCH_DESC'] = 'Lasterketa bilatzaile aurreratua izena, data, mota, distantzia eta beste parametro desberdinen bidez bilatzeko aukera ematen diguna.';
+$lang['APP_FUNCTIONS_PASS_RACES_TITLE'] = '<p>AURREKO LASTERKETAK<br/><small>Azken 15 egunetako lasterketak</small></p>';
+$lang['APP_FUNCTIONS_PASS_RACES_DESC'] = 'Aukeratutako lurraldearen azkenengo 15 egunetako lasterketak erakusteko. Era lehenetsian, lurralde guztietako lasterketak erakutsiko zaizkizu.';
+$lang['APP_FUNCTIONS_NEXT_RACES_TITLE'] = '<p>HURRENGO LASTERKETAK<br/><small>Hurrengo 30 egunetako lasterketak</small></p>';
+$lang['APP_FUNCTIONS_NEXT_RACES_DESC'] = 'Hurrengo 30 eguneko lasterketak emango zaizkizu aukeratutako lurraldean. Era lehenetsian, lurralde guztietako lasterketak erakutsiko zaizkizu.';
+$lang['APP_FUNCTIONS_NEAR_RACES_TITLE'] = '<p>INGURUKO LASTERKETAK<br/><small>Ingurukko lasterketen bilaketa</small></p>';
+$lang['APP_FUNCTIONS_NEAR_RACES_DESC'] = 'Gure kokapena kontutan hartuta inguruko lasterketak erakutsiko zaizkigu. Era lehenetsian 20km erradioko distantzian daudenak baina 20 lasterketa gutxienez aurkitzen ez badu, distantzia hau handituz joango da.';
+$lang['APP_FUNCTIONS_RACE_OPINIONS_TITLE'] = '<p>EDOZEIN LASTERKETA BURUZ IRITZIA EMAN<br/><small>Erraz eta minutu eskas baten</small></p>';
+$lang['APP_FUNCTIONS_RACE_OPINIONS_DESC'] = 'Servirace aplikazioan aurkitzen den edozein lasterketari buruzu iritzia ematea, bakoitzaren fitxan "Iritziak" estekaren bidez. Ezinbestekoa da izena ematea iritzia emateko.';
+$lang['APP_FUNCTIONS_ADD_RACE_TO_CALENDAR_TITLE'] = '<p>EGUTEGIAN GEHITU<br/><small>Aukeratutako abisuak jasotzeko.</small></p>';
+$lang['APP_FUNCTIONS_ADD_RACE_TO_CALENDAR_DESC'] = 'Lasterketa baten oinarrizko informazioa gehitzeko gure Google kontuan Google Calendar aplikaziotik abisuak jasotzeko zehaztutako konfigurazioarekin.';
+$lang['APP_FUNCTIONS_RACE_OPINION_ALERT_TITLE'] = '<p>GERTAKARI BERRIEN ABISUAK<br/><small>Push abisuak jaso</small></p>';
+$lang['APP_FUNCTIONS_RACE_OPINION_ALERT_DESC'] = 'Aplikazioan gertakari berriak ematean, hau da, aplikazioari buruzko berrikuntza bat, lasterketa berriak edota iritzi berriak daudenean, Push mezuak jasoko ditugu.';
+$lang['APP_FUNCTIONS_MORE_FUNCTIONS_TITLE'] = '<p>FUNTZIO GEHIAGO ESKURAGARRI<br/><small>Aurkitu aplikazioan</small></p>';
+$lang['APP_FUNCTIONS_MORE_FUNCTIONS_DESC'] = 'Si quieres descubrirlas descarga la aplicaci&oacute;n desde <a href="https://play.google.com/store/apps/details?id=com.amugika.servirace&hl=es" target="_blank">aqu&iacute;</a> que es <b><u>GRATIS</u></b>.';
+
+//FAQ Texts
+
+$lang['FAQ_TITLE'] = 'Galdera eta erantzunak aplikazioari buruz';
+$lang['FAQ_SUBTITLE'] = 'Kontu garrantzitsuenei buruz azalpenak daude.';
+$lang['FAQ_MORE_INFO'] = 'Beste zalantzarik izanez gero, <a href="mailto:servirace.app@gmail.com">idatzi posta</a> bat mesedez.';
+$lang['FAQ_WHAT_IS_SERVIRACE_TITLE'] = 'Zer da ServiRace?';
+$lang['FAQ_WHAT_IS_SERVIRACE_DESCRIPTION'] = '<p>Lasterketa baten parte hartzeko zalantzan zaude? Lasterketa zehatz bati buruz besteek pentsatzen dutena jakin nahi ahal zenuke? Lasterketa bati buruzko iritzia ematea gustatuko litzaizuke interesatuta eta  zalantzan daudenei laguntzeko asmoz?</p>
+
+<p>Hauetako galdera bati BAI erantzun baduzu zure aplikazioa dela esan dezakegu. Lasterketa askotako informazioa jasoko duen aplikazioa da Servirace App (etengabeko eguneratzeak dituena) xehetasun teknikoekin (lasterketa mota, km-ak, malda,…), informazioa orokorra (noiz ospatuko den edo ospatu izan den urte horren barruan, honen kokapena eta nola heldu ahal izango dugun Google Maps-aren bitartez gure gailuetan instalaturik daukagula), inguruan dauden zerbitzuak (tabernak, jatetxeak, garraio desberdinen aukerak,…) eta lasterketa horri buruzko erabiltzaileen iritziak kontsultatu ditzazkezu.</p>
+
+<p>Aplikazioaren puntu garrantzitsuena erabiltzaileen iritzietan eta lasterketa inguruan dauden zerbitzuak  (tabernak, hotelak,…) dira  non zerbitzuak gertuenetik urrunago dagoenera sailkatuko da kategoria desberdinetan filtratu ahal izango ditugularik.
+
+<p>Lasterketa pila bat gehitu dira urtarriletik. Euskal Herriko eta inguruko lasterketekin hasi ginen arren une honetan hauetaz aparte Iberiar Penintsulako eta Mundu Mailako lasterketak (Londres-eko maratoia, Ultra Trail Mont Blanc, Boston Maratoia...) gehitu dira datu basean.</p>
+
+<p>Momentuz 980 lasterketa baino gehiago daude eta hauei loturiko 88000 zerbitzu baino gehiago daude baina asmoa da ahalik eta lasterketa gehien gehitzea ahalik eta aplikazio osoena sortzeko asmoz.</p>
+
+<p>Hori bai, denbora librean egiten den proiektu bat da, dohainik eskaintzen dena erabiltzaileei eta asko eskertuko nizueeken zuen laguntza zuen inguruko edota parte hartuko duzuten lasterketak gehituko bazenute. Horrela lasterketen informazioa osoa edukiko genuen (eguraldi iragarpena, lasterketa xehetasunak, zerbitzuak,...) informazio guztia toki berean edukiz eskura.</p>
+
+<p>Ez du axola nongoa den lasterketa, frogak egin ondoren edozein kontinente nahiz herrialdeko lasterketak gehituta bere informazio guztia lortzea posible da (Googlek erregistraturik badu noski). Horregatik proposatu lasai!</p>
+
+<p>Aplikazioak dituen lasterketa zerrenda aurki argitaratuko dut webgune formatuan jakinaren gainean edukitzeko zer lasterketa dauden eta zeintzuk ez, zuen iradokizunak jaso ahal izateko.</p>
+
+<p>Norbaitek lasterketa zehatz bat gehitu nahi izanez gero gehitu informazioa hurrengo formularioan: <a href="https://docs.google.com/forms/d/1g6okx0F0rLf0q0JDy-ZBgLNK0jHqF2boLkWwYj6-4_M/viewform">https://docs.google.com/forms/d/1g6okx0F0rLf0q0JDy-ZBgLNK0jHqF2boLkWwYj6-4_M/viewform</a></p>
+
+<p>24-48 ordutan egongo dira ikusgai. Zuen e-posta jarriz gero zuekin jarriko naiz harremanetan jakiteko ea dena zuzen dagoen eta aldi berean nere esker ona emateko. Espero dut zuen gustokoa izatea!!</p>
+				                                <p>Aplikazioaren hizkuntzak:</p>
+
+				                                <ul>
+				                                    <li>Catalá (Itzulpena <b>Gerard Mestres</b> erabiltzaileak egindakoa, eskerrik asko!! 25/09/2015-tik erabilgarri)</li>
+				                                    <li>Español</li>
+				                                    <li>Euskara</li>
+				                                </ul>';
+$lang['FAQ_HOW_MUCH_SERVIRACE_TITLE'] = 'Zenbateko salneurria du aplikazioak?';
+$lang['FAQ_HOW_MUCH_SERVIRACE_DESCRIPTION'] = '<b><u>Ezer ez. 0 €.</u></b> Android sistema eragilea duen gailua (<b>Android 4.0 bertsio gutxienez</b>) eta Google Play dendako aplikazioa eguneratua izanda nahiko izango zen. Hau izanda <a href="https://play.google.com/store/apps/details?id=com.amugika.servirace" target="_blank">hemendik</a> jeitsi dezakegu aplikazioa.';
+$lang['FAQ_OTHER_VERSIONS_SERVIRACE_TITLE'] = 'Ez dago ez iOS ez Windows Phone bertsiorik, Zergatik?';
+$lang['FAQ_OTHER_VERSIONS_SERVIRACE_DESCRIPTION'] = 'Nahiz eta iOS gailuetarako aplikazioen garapenerako ezagutzak izan ez dut garatu gailu hauetarako bi arrazoi nagusiengandik, nahiz eta Android gailuak ez dutenentzat (iOS, Windows Phone,...) web bertsio bat garatzen ari naiz Servirace zerbitzuak disfrutatu dezaten:
+					                                <ul>
+					                                    <li>Finantziazio gabeko proiektu bat izanik iTunes garatzaile lizentziade 99$/urteko dira eta horren eraginez aplikazioan emandako inbertsio kostua asko igotzen du momentu honetan aplikazioaren datuak gordetzeko zerbitzari baten alokairuaren gastua dago eta.</li>
+					                                    <li>Denbora faltagatik, behar bat asetzeko helburuarekin jaio zen Servirace eta nire denbora librean garatu dudana pausoz-pauso.</li>
+					                                </ul>';
+$lang['FAQ_NO_FOUND_RACES_EXPLAIN_SERVIRACE_TITLE'] = 'Lasterketa bat ez du aurkitzen nahiz eta bilatu, nola egin dezaket aplikazioan agertzeko?';
+$lang['FAQ_NO_FOUND_RACES_EXPLAIN_SERVIRACE_DESCRIPTION'] = 'Ez kezkatu, zenbait aukera dituzu lasterketa aplikazioan agertzeko:
+							                                <ul>
+							                                    <li>Aplikazio beretik, ezkerreko menu nagusian <b>"Lasterketak proposatu"</b> aukeran.</li>
+							                                    <li>Hurrengo <a href="https://docs.google.com/forms/d/1g6okx0F0rLf0q0JDy-ZBgLNK0jHqF2boLkWwYj6-4_M/viewform" target="_blank">formularioa</a> erabilita.</li>
+							                                    <li><a href="mailto:servirace.app@gmail.com" target="_blank">servirace.app[arroba]gmail.com</a> helbidea erabilita lasterketaren informazioa bidaltzeko.</li>
+							                                </ul>';
+$lang['FAQ_REGISTER_USERS_ADVANTAGES_SERVIRACE_TITLE'] = 'Zer abantaila dauzka izena emandako erabiltzailea izateagatik?';
+$lang['FAQ_REGISTER_USERS_ADVANTAGES_SERVIRACE_DESCRIPTION'] = 'Hasiera batean abantaila asko ez izan arren, interesgarria da izena emanda edukitzea hurrengo eguneraketetan ekarriko diren funtzionalitateak hobeto aprobetxatu ahal izateko. Momentuz, gonbidatuekin alderatuz izango diren abantailak honako hauek dira:
+								                                 <ul>
+								                                    <li>Nahi duzun edozein lasterketaz iritzia eman ahal izatea (Hasiera batean iritzi bat urteko eta lasterketako aldatzeko aukerarik gabe. Iritziak aldatzeko funtzionalitatean lanean ari naiz, pazientzia mesdez).</li>
+								                                    <li>Gure erabiltzaile profilaren ezarpenak konfiguratu ahal izatea, gure oinarrizko datuetan eta jakinarazpenak jasotzeko ezarpenetan, aukeratutako lurraldea,...Zure erabiltzailearekin gordez gero, Servirace erabiliko duzun edozein gailuetan izango duzu konfigurazio hori ezarrita.</li>
+								                                </ul>';
+?>
